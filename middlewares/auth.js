d72d516524a9ee9055932199e3a942dfeb274108
sheldyrivaldi <sheldyrivaldi@gmail.com>
2022-10-05 15:30:20 +0700

    Milestone 3 : Drop Basic Auth and Switch to JWT

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,33 @@
 const express = require('express')
-const auth = require('basic-auth')
+// const auth = require('basic-auth')
+const jwt = require('jsonwebtoken')
 const app = express()
 require('dotenv').config()
 app.use(express.json())
 
 // Global Variable
-    const username = process.env.USERNAME_BASIC_AUTH
-    const password = process.env.PASSWORD_BASIC_AUTH
+    const jwtKey = process.env.JWT_KEY
 
-// Middleware with Basic Auth
-app.use('/',  (req, res, next)=> {
-if(auth(req) == undefined || auth(req).name !== username || auth(req).pass !== password){
-   return res.status(403).json({
-        message: "Not Authorized!"
-    }) 
-    
-} next()
-})
 
-module.exports = app
+// Middleware JWT
+function auth (req,res,next){
+    const header = req.header('Authorization')
+    const jwtKey = process.env.JWT_KEY
+    if(!header) return res.status(401).json({
+        message: "Access Denied!"
+    })
+     
+    try {
+        const token = header.split(' ')[1]
+        const verfication = jwt.verify(token,jwtKey)
+        req.user = verfication
+        next()
+    } catch {
+        res.status(400).json({
+            message: "Invalid Token!"
+        })
+    }
+        
+}
+
+module.exports = auth
--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../middlewares/auth')
+const jwt = require('jsonwebtoken')
 router.use(express.json())
-router.use(auth)
-
+require('dotenv').config()
 
 router.post('/', (req, res)=>{
-    res.status(200).json({
-        message: "Login Successfully!"
-    })
+    if(req.body.username == undefined || req.body.password == undefined){
+        res.status(400).json({
+            message: "Invalid Usename or Password!"
+            })
+    }else if(req.body.username != process.env.USERNAME_LOGIN || req.body.password != process.env.PASSWORD_LOGIN){
+        res.status(400).json({
+            message: "Wrong Usename or Password!" 
+            })
+    } else {
+        const user = req.body.username
+        const jwtKey = process.env.JWT_KEY
+        const token  = jwt.sign({username: user}, jwtKey)
+        res.header('Authorization', "Bearer " + token).status(200).json({
+            message: "Login Successfully!"
+        })
+    }
 })
+
 
 module.exports = router
